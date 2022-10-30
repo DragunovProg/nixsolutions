2b03dd411d2f631e164904c9b67e088b79183b96
dragunov <igor.dragunov99@gmail.com>
2022-10-30 14:44:49 +0200

    fix reflection module

---
+++ dining-philosophers/src/main/java/ua/dragunov/DiningPhilosophersHandler.java
@@ -0,0 +1,48 @@
+package ua.dragunov;
+
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Util class executing a concurrency algorithm to feeding all philosophers  around the table
+ * */
+public class DiningPhilosophersHandler {
+    private Lock leftForkLock = new ReentrantLock();
+    private Lock rightForkLock = new ReentrantLock();
+
+    public void dining(int philosopher,
+                       Runnable pickLeftFork,
+                       Runnable pickRightFork,
+                       Runnable eat,
+                       Runnable putLeftFork,
+                       Runnable putRightFork) throws InterruptedException {
+
+
+        while(true) {
+            if(leftForkLock.tryLock()) {
+                try {
+                    pickLeftFork.run();
+
+                    if (rightForkLock.tryLock()) {
+                        try {
+                            pickRightFork.run();
+                            eat.run();
+                            putRightFork.run();
+
+                            return;
+                        } finally {
+                            rightForkLock.unlock();
+                        }
+                    }
+
+
+                } finally {
+                    putLeftFork.run();
+                    leftForkLock.unlock();
+                }
+            }
+        }
+    }
+}
